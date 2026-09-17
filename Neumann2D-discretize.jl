3bc119dc250b379caef79c7e58524a75685fc25b
vietbuiminh <buiminhviet.2002@gmail.com>
2026-09-17 12:38:09 -0400

    Modify file and this is at the end of the class on Thurs

--- Neumann2D-discretize.jl
+++ Neumann2D-discretize.jl
@@ -25,10 +25,18 @@ hR = 9 # right boundary head [m] [Dirichlet type]
 # transmissibility matrix
 # in class the code contain an input file for Kmat, but here we just use a constant
 # value for Kmat
-Kmat = ρ*g*k/μ .* ones(ny, nx) # m/s <--- this is K * matrix
-
 # TODO: read in Kmat from a file, and then calculate the harmonic mean in x and y
 # directions
+kmat = readmatrix("data/Kmat.csv") # read in Kmat from a file
+# Kmat = ρ*g*k/μ .* ones(ny, nx) # m/s <--- this is K * matrix
+Kmat = ρ*g*kmat/μ .* kmat
+
+kmean = mean(log10(reshape(kmat, nc)))
+kstd = std(log10(reshape(kmat, nc)))
+
+Y = (log10(reshape(kmat, nc)) - kmean) / kstd
+kmat = kmean .+ 0.1 .* Y # 0.1 or 1.0 and make some comment
+Kmat = ρ*g*kmat/μ .* 10 .^ kmat
 
 K_har_x = (1 ./ Kmat[:, 1:(nx - 1)] .+ 1 ./ Kmat[:, 2:nx]) .^ -
 1 # harmonic mean in x direction
@@ -51,10 +59,10 @@ TDmat = fill(NaN, ny, nx)
 TDmat[ny, :] .= 0.0 # bottom boundary: no transmissibility
 TDmat[1:(ny - 1), :] .= (2/dx*dy) .* K_har_y # transmissibility in y direction
 
-TL = reshape(TLmat, nx*ny)
-TR = reshape(TRmat, nx*ny)
-TU = reshape(TUmat, nx*ny)
-TD = reshape(TDmat, nx*ny)
+TL = reshape(TLmat, nc)
+TR = reshape(TRmat, nc)
+TU = reshape(TUmat, nc)
+TD = reshape(TDmat, nc)
 
 TC = TL .+ TR .+ TU .+ TD # transmissibility matrix diagonal
 
@@ -77,3 +85,40 @@ h = Tmat \ b # solve for hydraulic head
 
 hmat = reshape(h, ny, nx) # reshape h vector to matrix form
 heatmap(hmat, xlabel = "x cell", ylabel = "y cell", title = "Hydraulic head")
+
+# TODO make the subplot of 2 where the first show the kmat and the bottom is the result
+# of hmat
+p = plot(layout = (2, 1))
+heatmap!(
+    p[1],
+    log10(Kmat),
+    xlabel = "x cell",
+    ylabel = "y cell",
+    title = "Log10(Permeability)",
+)
+heatmap!(p[2], hmat, xlabel = "x cell", ylabel = "y cell", title = "Hydraulic head")
+
+# Darcy velocity 
+
+dh_x = hmat[:, 1:(nx - 1)] .- hmat[:, 2:nx] # head difference in x direction
+dh_y = hmat[1:(ny - 1), :] .- hmat[2:ny, :] # head difference in y direction
+
+UL = fill(NaN, ny, nx)
+UL[:, 1] .= u_in # left boundary: specified flux (Neumann)
+UL[:, 2:nx] .= TLmat[:, 2:nx] .* dh_x ./ dx # Darcy velocity in x direction
+
+UR = fill(NaN, ny, nx)
+UR[:, nx] .= TRmat[:, nx] .* dh_x[:, nx -
+1] ./ dx # right boundary: specified head (Dirichlet)
+UR[:, 1:(nx - 1)] .= TRmat[:, 1:(nx - 1)] .* dh_x ./ dx # Darcy velocity in x direction
+
+UU = fill(NaN, ny, nx)
+UU[1, :] .= 0.0 # top boundary: no transmissibility
+UU[2:ny, :] .= TUmat[2:ny, :] .* dh_y ./ dx # Darcy velocity in y direction
+
+UD = fill(NaN, ny, nx)
+UD[ny, :] .= 0.0 # bottom boundary: no transmissibility
+UD[1:(ny - 1), :] .= TDmat[1:(ny - 1), :] .* dh_y ./ dx # Darcy velocity in y direction
+
+Ux = 0.5 * (UL .+ UR) # average Darcy velocity in x direction
+Uy = 0.5 * (UU .+ UD) # average Darcy velocity in y direction
